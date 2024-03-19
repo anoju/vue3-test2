b0788d400d0f33d45c0cb98905ac980d9ae7fcb3
anoju <zun5761@gmail.com>
2024-03-19 09:30:00 +0900

    scss 적용

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    sourceMap: true,
+    // loaderOptions: {
+    //   sass: {
+    //     prependData: '@import "~@/assets/scss/_function.scss";@import "~@/assets/scss/_mixin.scss";'
+    //   },
+    // }
+  }
 })

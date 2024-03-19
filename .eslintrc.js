f10365315bfa7280821d4370fa299d6e6420bf5f
anoju <zun5761@gmail.com>
2024-03-19 09:35:22 +0900

    etc setting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    // 'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-param-reassign': 'off',
+    'linebreak-style': 'off',
+    'no-alert': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: ['error', 'never'],
+    //'comma-dangle': ['error', 'never'],
+    'comma-dangle': 'error',
+    'comma-spacing': 'error',
+    'eol-last': ['error'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'], // 문자열따옴표 설정(single / double)
+    'vue/html-indent': ['error', 2],
+    'max-len': ['error', { code: 400 }],
+    'fun-names': 'off',
+  },
+};
